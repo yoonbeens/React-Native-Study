ae482eb3c47917b41613955634cc059517af22b1
yoon <cunbn@naver.com>
2023-07-13 10:24:12 +0900

    # 1. React-Native 초기 설정 및 개념 학습

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder='할 일을 입력하세요!' />
+        <Button title='할 일 추가하기' />
+      </View>
+      <View>
+        <Text>할 일 목록들...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50,
   },
 });
